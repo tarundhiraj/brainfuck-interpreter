339ce17ca3eb854304aef4dbbecdb0c5061d3c16
Sohil <supertakumi86@gmail.com>
2017-10-06 00:27:18 +0530

    Created Brainfuck Interpreter

---
+++ backend/brainfuck_interpreter_test.py
@@ -0,0 +1,5 @@
+from brainfuck_interpreter import BrainfuckProgram
+
+source = '+++[>+++++<-]>[>+>+++>+>++>+++++>++<[++<]>---]>->-.[>++>+<<--]>--.--.+.>>>++.<<.<------.+.+++++.>>-.<++++.<--.>>>.<<---.<.-->-.>+.[+++++.---<]>>[.--->]<<.<+.++.++>+++[.<]'
+program = BrainfuckProgram(source)
+print program.run()
